da8de80917f4b29f32afd914f21070bca7465bb9
JLGGG <blockboy2957@gmail.com>
2023-10-11 22:25:49 +0900

    thread guard

--- thread1/src/aver.cpp
+++ thread1/src/aver.cpp
@@ -7,6 +7,21 @@
 
 using namespace std;
 
+class ThreadGuard {
+private: 
+    thread &thr;
+public:
+    explicit ThreadGuard(thread &t) : thr(t) {}
+    ~ThreadGuard() {
+        static int i = 1;
+        if(thr.joinable()) thr.join();
+        fmt::print("ThreadGuard destructor: {}\n", i++);
+    }
+
+    ThreadGuard & operator=(const ThreadGuard &o) = delete;
+    ThreadGuard(const ThreadGuard &o) = delete;
+};
+
 struct PartialSumFunctor {
     vector<double>::iterator start;
     vector<double>::iterator end;
@@ -22,33 +37,32 @@ void PartialSumFunctor::operator() () {
 }
 
 int main(int argc, char **argv) {
-    int num_thr = atoi(argv[1]);
-    int N = atoi(argv[2]);
+    int num_thr = thread::hardware_concurrency();
+    int N = atoi(argv[1]);
     int step = (int)ceil(N*1.0/num_thr);
     // double res[num_thr];
 
     vector<double> data(N);
     iota(data.begin(), data.end(), 1);
     // thread *thr[num_thr];
-    unique_ptr<thread> thr[num_thr-1];
+    unique_ptr<thread> thr[num_thr];
+    unique_ptr<ThreadGuard> tg[num_thr];
     PartialSumFunctor f[num_thr];
     vector<double>::iterator local_start = data.begin();
     vector<double>::iterator local_end;
-    for (int i = 0; i < num_thr - 1; i++) {
+    for (int i = 0; i < num_thr; i++) {
         local_end = local_start + step;
+        if (i==num_thr-1) local_end = data.end();
         f[i].start = local_start;
         f[i].end = local_end;
         thr[i] = make_unique<thread>(ref(f[i]));
+        tg[i] = make_unique<ThreadGuard>(ref(*thr[i]));
         local_start += step;
     }
 
-    f[num_thr - 1].start = local_start;
-    f[num_thr - 1].end = data.end();
-    f[num_thr - 1]();
-
-    double total = f[num_thr - 1].res;
-    for (int i = 0; i < num_thr-1; i++) {
-        thr[i]->join();
+    double total = 0;
+    for (int i = 0; i < num_thr; i++) {
+        // thr[i]->join();
         total += f[i].res;
     }
 
